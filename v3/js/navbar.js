62e03f420018d79be663fbed6b8c9b1c14b1f27f
Wobblyyyy <crobertson22@students.roxbury.org>
2019-12-13 22:04:11 +0000

    n

---
+++ v3/js/navbar.js
@@ -0,0 +1,44 @@
+var selection = document.getElementsByClassName("Navbar_Item_Left");
+for (let i = 0; i < selection.length; i++)
+{
+  selection[i].addEventListener("click", function (rte)
+  {
+    if (selection[i].classList.contains(".Navbar_Item_Left"))
+    {
+      [].forEach.call(document.querySelectorAll(".Navbar_Item_Left"), function (element)
+      {
+        if (element.classList.contains("w3-text-teal"))
+        {
+          element.classList.remove("w3-text-teal");
+        }
+      });
+      selection[i].classList.add("w3-text-teal");
+      document.body.classList.add("fadeOut");
+    }
+  });
+}
+
+function alreadySignedIn ()
+{
+  _("#Button_Login").addClass("disabled");
+  _("#Button_Login").removeClass("Navbar_Item_Left");
+  document.getElementById("Button_Login").href = "javascript:;";
+  _("#Button_Register").addClass("disabled");
+  _("#Button_Register").removeClass("Navbar_Item_Left");
+  document.getElementById("Button_Register").href = "javascript:;";
+  _("$sidebar").append('<a id="Button_About" href="logout.html" class="Navbar_Item Navbar_Item_Left w3-bar-item w3-button w3-padding fadeIn"><i class="fa fa-th-large fa-ban w3-margin-right"></i>sign out</a>');
+  _("$sidebar").append('<a id="Button_About" href="app/homepage.html" class="Navbar_Item Navbar_Item_Left w3-bar-item w3-button w3-padding fadeIn"><i class="fa fa-th-large fa-home w3-margin-right"></i>homepage</a>');
+}
+
+firebase.auth().onAuthStateChanged(function (user)
+{
+  if (user)
+  {
+    alreadySignedIn();
+  }
+});
+
+if (Cookies.get("username").length > 0)
+{
+  alreadySignedIn();
+}
