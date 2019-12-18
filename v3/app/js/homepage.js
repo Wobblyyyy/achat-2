21d2f4d63c75dc78e037ddfdc9db34d53d8ed5f3
Wobblyyyy <30844830+Wobblyyyy@users.noreply.github.com>
2019-12-17 22:30:54 -0500

    cool

--- v3/app/js/homepage.js
+++ v3/app/js/homepage.js
@@ -2,3 +2,15 @@ _("#signout").on("click", function ()
 {
   window.location.href = "../logout.html";
 });
+
+function closeModal ()
+{
+  document.getElementById("modal").display = "none";
+  document.getElementById("pagecover").display = "none";
+}
+
+function openModal ()
+{
+  document.getElementById("modal").display = "block";
+  document.getElementById("pagecover").display = "block";
+}
